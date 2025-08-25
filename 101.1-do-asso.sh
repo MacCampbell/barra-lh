5460b1237235fbc373f299585d1feebca35e00ef
Mac Campbell <campbellmatthewa@gmail.com>
2025-08-25 12:45:07 +1000

    setting up association of test data

--- 101.1-do-asso.sh
+++ 101.1-do-asso.sh
@@ -2,9 +2,9 @@
 
 # We can generate per chrom files with a loop like this to speed things up
 # Feeding a list of chroms meta/test.chroms
-# bash $HOME/shernook/202-do-asso.sh $HOME/shernook/bamlists/bamlist56.bamlist $HOME/shernook/meta/test.chroms $HOME/shernook/bamlists/bamlist56-early-late.pheno
+# `bash $HOME/barra-lh/101.1-do.asso.sh $HOME/barra-lh/bamlists/test44.bamlist $HOME/barra-lh/meta/lates-lgs.txt  $HOME/barra-h/bamlists/test44.phenos`
 
-# To do: generalize a bit more so that 1/2 of bamlist is specified for -minInd
+# Generalized so that 1/2 of bamlist is specified for -minInd
 
 bamlist=$1
 list=$2
@@ -16,11 +16,11 @@ thresh=$((lines/2))
 
 while read chrom; do
   echo "#!/bin/bash -l
-  $HOME/angsd/angsd -doAsso 1 -yBin $phenos -GL 1 -nThreads 12 -minInd $thresh  \
+  $HOME/angsd/angsd -P 12 -doAsso 1 -yBin $phenos -GL 1 -minInd $thresh  \
    -minMapQ 20 -minQ 20 \
   -doMajorMinor 1 -doMaf 1 -SNP_pval 1e-6 -r $chrom -out $chrom-asso \
   -bam $bamlist  > $chrom-asso.out 2> $chrom-asso.err " > $chrom-asso.sh
   
-sbatch -p high -t 48:00:00 --mem=32G --nodes=2 $chrom-asso.sh
+sbatch -p high -t 24:00:00 --mem=32G --nodes=1 --cpus-per-task=12 $chrom-asso.sh
 
 done < $list
